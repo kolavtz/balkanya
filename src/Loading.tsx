faebaf7b8521dabe31dd0f0d456221a4de141d34
Hari Bhandari <156830509+kolavtz@users.noreply.github.com>
2026-01-07 20:18:12 +0530

    Create Loading component

---
+++ src/Loading.tsx
@@ -0,0 +1,13 @@
+import './Loading.css';
+
+export default function Loading() {
+  return (
+    <div className="loading-screen">
+      <div className="loading-container">
+        <div className="loader"></div>
+        <h1>Loading Balkanya School</h1>
+        <p>Please wait while we prepare your experience...</p>
+      </div>
+    </div>
+  );
+}
